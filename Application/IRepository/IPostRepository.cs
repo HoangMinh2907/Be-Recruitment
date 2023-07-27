327a02888ce01297413b2da5a6168947755124fe
Hoang Minh <Minhpew2907@gmail.com>
2023-07-27 12:50:46 +0700

    nho anh khoi giup

---
+++ Application/IRepository/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using WebRecruitment.Application.IRepository.IGeneric;
+using WebRecruitment.Application.Model.Response.PostResponse;
+using WebRecruitment.Domain.Entity;
+
+namespace WebRecruitment.Application.IRepository;
+
+public interface IPostRepository : IGenericRepository<Post>
+{
+    Task<List<Post>> GetPostByNameSkill(Guid jobId);
+    Task<List<Post>> GetPostByCompanyID(Guid companyId);
+    Task<List<Post>> GetPostByLocation(string location);
+    Task<List<Post>> GetPostBySalary(double salary);
+    Task<Post> GetAllPostDetail(Guid id);
+    Task<List<Post>> GetAllPost();
+}
